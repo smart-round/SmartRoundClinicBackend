846782fd486178e36af39e43e71c9bd781434b75
Dev-Pasaka <lazarusmutuku76@gmail.com>
2026-05-03 23:18:40 +0300

    returning all banks

--- common/src/main/kotlin/ke/co/smartroundclinic/common/OtpCodeGenerator.kt
+++ common/src/main/kotlin/ke/co/smartroundclinic/common/OtpCodeGenerator.kt
@@ -7,7 +7,7 @@ class OtpCodeGenerator {
     // SecureRandom is cryptographically strong unlike Random or Math.random()
     private val secureRandom = SecureRandom.getInstanceStrong()
 
-    fun generateOtpCode(length: Int = 6): String {
+    fun generateOtpCode(length: Int = 4): String {
         return (1..length)
             .map { secureRandom.nextInt(10) } // 0-9 only
             .joinToString("")
